faa09379fc1b3319d82a1406d203fc44d2d2b35a
Z M Asif Jamil <80680790+FurySimo@users.noreply.github.com>
2021-03-15 20:18:48 +0600

    Add files via upload

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Its a different line.");
+            Console.Write("Hello World!");
+            Console.Write("Its prited on same line.");
+        }
+    }
+}
